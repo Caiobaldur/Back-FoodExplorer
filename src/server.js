55da7d9fb70c64ae461bdeecfa3a741ae274adab
Caio Balduino <caiobrafachino@hotmail.com>
2023-11-23 23:17:44 -0300

    [test]: testing post method.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-  const {id, user} = request.params;
-
-  response.send(`Hello World! ${id}. Usuário ${user}`);
+app.post("/users", (request, response) => {
+  const{name, email, password} = request.body
+  response.json({name, email, password});
 });
 
 const PORT = 3333;
