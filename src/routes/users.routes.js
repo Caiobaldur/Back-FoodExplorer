a86a0293e9f4f0908378178362f0a0ce9cffe202
Caio Balduino <caiobrafachino@hotmail.com>
2023-12-15 13:32:27 -0300

    [feat] adding middleware function

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,7 +4,19 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
+// function myMiddleware(request, response, next){
+//   if(!request.body.isAdmin){
+//     return response.json({message: "Usuário não autorizado!"})
+//   }
+
+//   next();
+// }
+
+
+
 const usersController = new UsersController();
+
+// usersRoutes.post("/", myMiddleware, usersController.create);
 
 usersRoutes.post("/", usersController.create);
 
