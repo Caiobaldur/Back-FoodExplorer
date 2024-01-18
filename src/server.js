a022cf473b749f1a7bcc6f903ddc3bb464aab0a8
Caio Balduino <caiobrafachino@hotmail.com>
2024-01-18 15:13:05 -0300

    fast changes

--- src/server.js
+++ src/server.js
@@ -26,4 +26,6 @@ app.use((error, request, response, next) => {
 
 const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+app.listen(PORT, () => {
+ console.log(`Listen on Port ${PORT}`)
+});
