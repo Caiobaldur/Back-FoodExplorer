bac84abd1f6dae9b0400e054801e92e5e7e5f84c
Caio Balduino <caiobrafachino@hotmail.com>
2024-01-18 17:51:13 -0300

    [feat] functionality Updating password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/AppError.js");
 const sqliteConnection = require("../database/sqlite");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 class UsersController {
   async create(request, response) {
@@ -32,7 +32,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
@@ -54,13 +54,28 @@ class UsersController {
     user.name = name;
     user.email = email;
 
+    if (password && !old_password) {
+      throw new AppError("Informe a senha antiga para alterar a senha");
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if(!checkOldPassword) {
+        throw new AppError("A senha antiga não confere!")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
     await database.run(
       `UPDATE users SET 
       name = ?, 
-      email = ?, 
+      email = ?,
+      password = ?, 
       updated_at = ? 
       WHERE ID = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email,user.password, new Date(), id]
     );
 
     return response.status(200).json();
